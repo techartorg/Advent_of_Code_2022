eacef9b675937bea4ecf978ee8fbdc2cba35fd42
Jeff Hanna <jeff.b.hanna@gmail.com>
2022-11-20 07:15:30 -0600

    Boilerplate, prepping for day 1

---
+++ jeff_hanna/_template.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+<DAY_##_PUZZLE_DESCRIPTION>
+"""
+from contextlib import suppress
+from pathlib import Path
+from typing import Callable, Dict, List, Optional, Tuple
+
+def
+
+
+if __name__ == '__main__':
