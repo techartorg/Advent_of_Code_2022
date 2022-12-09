1c22a1ce9d8dd380dcd2dfdce1d6e18ab13102dd
Chris Cunningham <chris@rightsomegoodgames.ca>
2022-12-08 20:06:49 -0400

    a greatly cleaned up soultion for day 08

--- chris_cunningham/day_08.py
+++ chris_cunningham/day_08.py
@@ -1,68 +1,40 @@
 from pathlib import Path
-from typing import Callable
+from math import prod
+from typing import Iterator
 
 inputs = Path(__file__.replace(".py", ".input")).read_text().splitlines()
 
-Grid = list[list[int]]
-Pt = tuple[int, int]
+Point = tuple[int, int]
+Grid = dict[Point, int]
+directions = ((-1, 0), (1, 0), (0, 1), (0, -1))
+
+grid = {(x, y): height for y, row in enumerate(inputs) for x, height in enumerate(row)}
 
 
-def find_visible(data: Grid) -> set[Pt]:
-    cols = len(data[0])
-    rows = len(data)
-    seen: set[tuple[int, int]] = set()
-
-    for y in range(rows):
-        for it in [range(cols), reversed(range(cols))]:
-            highest_seen = -1
-
-            for x in it:
-                if data[y][x] <= highest_seen:
-                    continue
-
-                highest_seen = data[y][x]
-                seen.add((x, y))
-
-    for x in range(cols):
-        for it in [range(rows), reversed(range(rows))]:
-            highest_seen = -1
-
-            for y in it:
-                if data[y][x] <= highest_seen:
-                    continue
-
-                highest_seen = data[y][x]
-                seen.add((x, y))
-
-    return seen
+def ray(start: Point, direction: Point) -> Iterator[Point]:
+    x, y = start
+    step_x, step_y = direction
+    while (x := x + step_x, y := y + step_y) in grid:
+        yield x, y
 
 
-def calc_scenic_score(data: Grid, point: Pt) -> int:
-    x, y = point
-    height = data[y][x]
-    rows = len(data)
-    cols = len(data[0])
-
-    def find(r: range, get_height: Callable[[int], int]) -> int:
-        v = 0
-        for c, i in enumerate(r):
-            v = c + 1
-            if get_height(i) >= height:
-                break
-        return v
-
-    right = find(range(x + 1, cols), lambda it: data[y][it])
-    left = find(range(x - 1, -1, -1), lambda it: data[y][it])
-    down = find(range(y + 1, rows), lambda it: data[it][x])
-    up = find(range(y - 1, -1, -1), lambda it: data[it][x])
-
-    return right * left * down * up
+def is_visible(start: Point) -> bool:
+    return any(all(grid[p] < grid[start] for p in ray(start, d)) for d in directions)
 
 
-grid = [[int(col) for col in row] for row in inputs]
+def view(start: Point, direction: Point) -> int:
+    v = 0
+    for i, p in enumerate(ray(start, direction)):
+        v = i + 1
+        if grid[p] >= grid[start]:
+            break
+    return v
 
-visible_trees = find_visible(grid)
+
+def score(point: Point) -> int:
+    return prod(view(point, d) for d in directions)
+
+
+visible_trees = [p for p in grid if is_visible(p)]
 print(f"Part One: {len(visible_trees)}")
-
-scenic_scores = [calc_scenic_score(grid, i) for i in visible_trees]
-print(f"Part Two: {max(scenic_scores)}")
+print(f"Part Two: {max(score(p) for p in visible_trees)}")
